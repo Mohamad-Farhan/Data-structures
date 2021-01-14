f7f5d2e35cd5b9660519a76fc6d50ec4ad5dd8dd
tetra-shayeb <mohammednm40@gmail.com>
2021-01-14 13:19:08 +0200

    solved problem solving Q09

--- Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q09/main.js
+++ Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q09/main.js
@@ -6,11 +6,27 @@ console.log('Problem Solving Q: 9');
 Write a function that returns the length of the longest sequence of 1s in an array of 1s and 0s.
 */
 
-function longestSequence() {
-  // YOUR CODE HERE
+function longestSequence(arr) {
+  let number = 0;
+  let arrNumber = [];
+  arr.forEach((e) => {
+    if (1 === e) {
+      return number += 1;
+
+    } else {
+      arrNumber.push(number);
+      number = 0;
+    };
+  });
+  let result = Math.max(...arrNumber);
+  if (result) {
+    return result;
+  } else {
+    return "There is no one sequenced"
+  };
 }
 
-/* 
+/*
 Examples:
 longestSequence([0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1]) // => 4
 longestSequence([1, 1, 0, 1, 1, 0, 1, 0, 1, 1]) // => 2
