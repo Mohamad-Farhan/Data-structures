8b1468e57c8c0ece2803d28ce04e48cde3de9c01
tetra-shayeb <mohammednm40@gmail.com>
2021-01-14 15:24:37 +0200

    solved problem solving Q013

--- Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q13/main.js
+++ Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q13/main.js
@@ -6,28 +6,43 @@ console.log('Problem Solving Q: 13');
 Write a function that calculates the Highest possible sum of any n consecutive numbers in an array arr of integers (where n ≥ 1).
 */
 
-function findHighestPossibleSum() {
-  // YOUR CODE HERE
+function findHighestPossibleSum(arr, n) {
+  let largestSum = Number.NEGATIVE_INFINITY;
+  let sum = 0
+  let index = 0
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i]
+    index += 1
+    if (index === n) {
+      if (sum > largestSum) {
+        largestSum = sum;
+      }
+      index = 0
+      sum = 0
+      i -= n - 1;
+    }
+  }
+  return largestSum;
 }
 
-/* 
+/*
 findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 1) // => 10
 Examples:
-findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 2) // => 14 
-findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 3) // => 16 
-findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 4) // => 20 
-findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 5) // => 23 
+findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 2) // => 14
+findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 3) // => 16
+findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 4) // => 20
+findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 5) // => 23
 
-findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 1) // => 10 
-findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 2) // => 14 
-findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 3) // => 22 
-findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 4) // => 26 
-findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 5) // => 29 
+findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 1) // => 10
+findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 2) // => 14
+findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 3) // => 22
+findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 4) // => 26
+findHighestPossibleSum([5, 2, 9, 3, 10, 4, -7, 2, 6, 8], 5) // => 29
 
 
-findHighestPossibleSum([10, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 1) // => 10 
-findHighestPossibleSum([10, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 2) // => 19 
-findHighestPossibleSum([10, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 3) // => 22 
-findHighestPossibleSum([5, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 4) // => 32 
-findHighestPossibleSum([5, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 5) // => 36 
+findHighestPossibleSum([10, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 1) // => 10
+findHighestPossibleSum([10, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 2) // => 19
+findHighestPossibleSum([10, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 3) // => 22
+findHighestPossibleSum([5, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 4) // => 32
+findHighestPossibleSum([5, 2, 10, 9, 3, 10, 4, -7, 2, 6, 8], 5) // => 36
 */
