b1cb5566d84516e21d6a63d0474b4a5fc75f0ef0
tetra-shayeb <mohammednm40@gmail.com>
2021-01-14 12:37:08 +0200

    solved problem solving Q05

--- Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q05/main.js
+++ Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q05/main.js
@@ -6,8 +6,8 @@ console.log('Problem Solving Q: 5 ');
 Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
 */
 
-function findIntersection(strArr) {
- let firstString = strArr[0]
+const findIntersection = (strArr) => {
+  let firstString = strArr[0]
   let secondString = strArr[1]
 
   let firstElementArray = []
@@ -18,13 +18,13 @@ function findIntersection(strArr) {
   secondString.split(',').map((oneNumber) => {
     secondElementArray.push(Number(oneNumber))
   })
-  let myAnswer = (secondElementArray.filter(e =>      
-   firstElementArray.includes(e))).toString()
-return myAnswer
-  
+  let myAnswer = (secondElementArray.filter(e =>
+    firstElementArray.includes(e))).toString()
+  return myAnswer
+
 }
 
-/* 
+/*
 Examples:
 findIntersection(['1, 3, 4, 7, 13', '1, 2, 4, 13, 15']); // => '1,4,13'
 findIntersection(['1, 3, 9, 10, 17, 18', '1, 4, 9, 10']); // => '1,9,10'
