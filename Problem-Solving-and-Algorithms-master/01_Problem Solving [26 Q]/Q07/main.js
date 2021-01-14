089d1c3148e2d1b6b4c081c09b7273b85b6cd0ee
tetra-shayeb <mohammednm40@gmail.com>
2021-01-14 12:52:57 +0200

    solved problem solving Q07

--- Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q07/main.js
+++ Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q07/main.js
@@ -7,10 +7,16 @@ Write a function that returns the sum of odd numbers in an array.
 */
 
 function SumOddNumber() {
-  // YOUR CODE HERE
+  const sumNumber = arguments[0].filter((odd) => {
+    return odd % 2 !== 0
+  });
+  const result = sumNumber.reduce((previous, current) => {
+    return previous + current
+  }, 0);
+  return result;
 }
 
-/* 
+/*
 Examples:
 SumOddNumber([1, 2, 3, 4, 5]) // => 9
 SumOddNumber([1, 5, 3, 4, 5]) // => 14
