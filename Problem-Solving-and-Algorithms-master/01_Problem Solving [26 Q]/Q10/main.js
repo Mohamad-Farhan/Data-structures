d435f395d72875741fdba62100b4414cc7a2d4d0
tetra-shayeb <mohammednm40@gmail.com>
2021-01-14 13:22:36 +0200

    solved problem solving Q010

--- Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q10/main.js
+++ Problem-Solving-and-Algorithms-master/01_Problem Solving [26 Q]/Q10/main.js
@@ -6,11 +6,26 @@ console.log('Problem Solving Q: 10');
 Write a function that returns the number of occurrences of the most frequent character in a sentence.
 */
 
-function timesOfMostFreqChar() {
-  // YOUR CODE HERE
+function timesOfMostFreqChar(sentence) {
+  let object = {};
+  sentence.split("").forEach((c) => {
+    if (object[c]) {
+      object[c] += 1;
+    } else {
+      object[c] = 1;
+    }
+  });
+  let max = Number.NEGATIVE_INFINITY;
+  for (const c in object) {
+    if (object[c] > max) {
+      max = object[c];
+    }
+  }
+  return max;
 }
 
-/* 
+
+/*
 Examples:
 answer1('hello world') // => 3
 answer1('hello world lol') // => 5
