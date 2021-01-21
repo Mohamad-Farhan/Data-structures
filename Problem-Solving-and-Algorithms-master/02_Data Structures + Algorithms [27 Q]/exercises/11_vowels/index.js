aa40b7a4183caa919d3ad7c3c31263ef35dfb040
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-21 16:53:04 +0200

    solved Data structures Q11

--- Problem-Solving-and-Algorithms-master/02_Data Structures + Algorithms [27 Q]/exercises/11_vowels/index.js
+++ Problem-Solving-and-Algorithms-master/02_Data Structures + Algorithms [27 Q]/exercises/11_vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let vowel_list = 'aeiouAEIOU';
+    let vcount = 0;
+
+    for (let x = 0; x < str.length; x++) {
+        if (vowel_list.indexOf(str[x]) !== -1) {
+            vcount += 1;
+        }
+
+    }
+    return vcount;
+}
 
 module.exports = vowels;
